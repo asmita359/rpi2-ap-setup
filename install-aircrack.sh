662e0f7759467ca1a8f4e7b9335c9f31f9a9995b
Ishan <ishanism@gmail.com>
2018-05-09 05:12:21 +1000

    Update install-aircrack.sh
    Added 'airodump-ng-oui-update' command

--- install-aircrack.sh
+++ install-aircrack.sh
@@ -1,7 +1,9 @@
-apt-get install libssl-dev shtool libtool pkg-config autoconf automake
+sudo apt-get install libssl-dev shtool libtool pkg-config autoconf automake
 wget https://download.aircrack-ng.org/aircrack-ng-1.2.tar.gz
 tar -xzf aircrack-ng-1.2.tar.gz
 autoreconf -i
 ./configure
 make
 sudo make install
+
+sudo airodump-ng-oui-update
